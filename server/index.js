c84d91a33c285de1722d89dd032a25646762b805
belfa <miguel36410@gmail.com>
2017-01-27 20:31:24 +0100

    Primer Mensaje: Bienvenida
    Creamos nuestra aplicación de cliente en main.js y mostramos nuestro
    primer mensaje: el de bienvenida.

--- server/index.js
+++ server/index.js
@@ -9,9 +9,15 @@ app.get('/hola-mundo', function(resquest, response){
     response.status(200).send('Hola Mundo desde una ruta de prueba.');
 });
 
+var messages = [{
+    id : 1,
+    text: 'Bienvenido al chat privado de Socket.io y NodeJS de Patiño.....',
+    nickname: 'Bot - server'
+}];
+
 io.on('connection', function(socket){
     console.log("El nodo con IP: "+socket.handshake.address+" se ha conectado....");
-
+    socket.emit('messages',messages);
 });
 
 server.listen(6677, function(){
