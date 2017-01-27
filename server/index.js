afa966fa9931af29e292c9573893e9e695169014
belfa <miguel36410@gmail.com>
2017-01-27 20:04:35 +0100

    Base del cliente
    Cargamos con Express la página de nuestro chat como un Middleware
    estático y definimos su estructura básica.

--- server/index.js
+++ server/index.js
@@ -3,8 +3,15 @@ var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
+app.use(express.static('client'));
+
 app.get('/hola-mundo', function(resquest, response){
     response.status(200).send('Hola Mundo desde una ruta de prueba.');
+});
+
+io.on('connection', function(socket){
+    console.log("El nodo con IP: "+socket.handshake.address+" se ha conectado....");
+
 });
 
 server.listen(6677, function(){
